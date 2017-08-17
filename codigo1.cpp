b440f4fbaca7c905ad074e071feec074e3592b8f
ALUMNO-D <ALUMNO-D@UCSP.AULAB>
2017-08-17 11:11:05 -0500

    seventh commit

--- codigo1.cpp
+++ codigo1.cpp
@@ -1,11 +1,14 @@
 #include <iostream.h>
 using namespace
 init main();
-{ int,a,b,c,d,e,suma;
-a=5;
-b=6;
-c=10;
-d=15;
-e=26;
-suma=a+b+c+d+e;
-return 0;}
+{
+	int,a,b,c,d,e,suma;
+		a=5;
+		b=6;
+		c=10;
+		d=15;
+		e=26;
+		suma=a+b+c+d+e;
+		return 0;
+		
+}
