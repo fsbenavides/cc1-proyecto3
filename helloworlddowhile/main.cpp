349bce0fd9db13bba5994dbca91563c9a407a59c
ALUMNO-D <ALUMNO-D@UCSP.AULAB>
2017-09-05 09:28:55 -0500

    commit 17

---
+++ helloworlddowhile/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    do
+    {
+     cout << "Hello world!\n";
+    }
+    while(n-->1);
+    return 0;
+}
---
+++ helloworldwhile/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    while(n-->0)
+        cout << "Hello world!\n";
+
+    return 0;
+}
